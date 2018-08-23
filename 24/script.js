d4c9b9417ca9f61c45f5ae3cc8d0eddf5fe29e0e
VictoriaNeborak <krasavchenko.net@gmail.com>
2018-08-23 13:18:40 +0300

    дз 24

---
+++ 24/script.js
@@ -0,0 +1,12 @@
+/*alert("Я люблю БОРЩ!");*/
+console.log("Я люблю БОРЩ!");
+var admin;
+var name;
+name = "Василий";
+admin = name;
+console.log(admin);
+var planet = "Земля";
+var guest = "Петя";
+var username = prompt("enter your name", name);
+console.log(username);
+
